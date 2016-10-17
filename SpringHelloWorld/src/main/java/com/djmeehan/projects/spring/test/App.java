3635be38742ca0db9b27a34d536e42d0729be756
djmeehan <derekmeehan@gmail.com>
2016-10-17 22:14:31 +0100

    Adding Cave of programming Spring example

---
+++ SpringHelloWorld/src/main/java/com/djmeehan/projects/spring/test/App.java
@@ -0,0 +1,19 @@
+package com.djmeehan.projects.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		
+		Person person = (Person) context.getBean("person");
+		person.speak();
+			
+	}
+
+}
---
+++ SpringHelloWorld/src/main/java/com/djmeehan/projects/spring/test/Person.java
@@ -0,0 +1,10 @@
+package com.djmeehan.projects.spring.test;
+
+public class Person {
+
+	public void speak() {
+		System.out.println("I'm a person");
+	}
+
+
+}
